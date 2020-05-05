88d11762341a12b1ce44f018d65a6df49f1a8de6
Angra, Ankit <ankit.angra@bellmedia.ca>
2020-05-05 18:00:22 -0400

    Added Base Application

---
+++ app/src/main/java/com/ankitangra/www/dagger_starter/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.ankitangra.www.dagger_starter;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}
