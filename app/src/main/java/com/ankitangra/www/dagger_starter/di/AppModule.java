420ea715069402717986e7ff6eed324a8f7fe4db
Angra, Ankit <ankit.angra@bellmedia.ca>
2020-05-06 23:38:28 -0400

    Added Singleton Scope

--- app/src/main/java/com/ankitangra/www/dagger_starter/di/AppModule.java
+++ app/src/main/java/com/ankitangra/www/dagger_starter/di/AppModule.java
@@ -11,23 +11,28 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions() {
         return RequestOptions.placeholderOf(R.drawable.ic_continue_watching_selected);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance (Application application, RequestOptions requestOptions) {
         return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable (Application application) {
         return ContextCompat.getDrawable(application, R.drawable.ic_continue_watching_selected);
